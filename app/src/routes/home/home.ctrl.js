b759d90f88c6e3526e236ad1cb06f296d6e21d96
Kwon JaeYun <lezhin01@gmail.com>
2024-02-19 19:00:10 +0900

    로그인 인증 만들기

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,11 +1,28 @@
-const hello = (req,res) => {
+
+const output = { 
+    home : (req,res) => {
     res.render('home/index');
-};
-const login = (req,res) => {
+    },
+    login : (req,res) => {
     res.render('home/login');
+    },
+};
+
+const users = {
+    id: ["권재윤","김만나","임승범"],
+    password: ["1234","123","12"],
+};
+
+const process = {
+    login: (req,res) => {
+        const id = req.body.id,
+        password = req.body.password;
+
+        console.log(id,password);
+    }
 };
 
 module.exports = {
-    hello,
-    login,
+    output,
+    process,
 }
