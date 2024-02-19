e9001982b310e3c3a3c8059fa17553794ae39f93
Kwon JaeYun <lezhin01@gmail.com>
2024-02-19 17:41:25 +0900

    돔을 이용하여 html 객체 저장

--- app/app.js
+++ app/app.js
@@ -1,17 +1,3 @@
-// hppt 모듈로 서버가동
-// const http = require('http');
-// const app = http.createServer((req,res) => {
-//     res.writeHead(200,{"Content-Type": "text/html; charset=utf-8"});
-//     if (req.url === '/') {
-//         res.end("여기는 루트입니다.");
-//     } else if (req.url === "/login") {
-//         res.end("여기는 로그인 화면입니다.");
-//     }
-// } );
-// app.listen(3001,()=>{
-//     console.log("http로 가동된 서버입니다.");
-// });
-
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
@@ -24,7 +10,7 @@ const home = require('./src/routes/home');
 app.set("views","./src/views");
 app.set("view engine", "ejs");
 
-
+app.use(express.static(`${__dirname}/src/public`));
 app.use("/",home); // use => 미들 웨어르 등록해주는 메서드.
 
 module.exports = app;
